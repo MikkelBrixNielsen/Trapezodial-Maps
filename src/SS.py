12ca5397dc57562e248cf6d0a3f49ac1ec7c0c8e
MLPT <mikkelbn24@live.dk>
2024-11-05 16:03:58 +0100

    maybe almost done with merging the middle stuff

--- src/SS.py
+++ src/SS.py
@@ -6,21 +6,27 @@ class Node:
         self.left = left # Left / above
         self.right = right # Right / below
 
+    # overwrites this node with the data from another node
+    def overwrite(self, data, left, right):
+        self.data = data
+        self.left = left
+        self.right = right
+
 class SearchStructure:
     def __init__(self, BB):
         self.root = Node(BB)
 
     # find next neighbor in regards to s_i
     def _next_neighbour(self, region, seg):
-        if len(region.rightN) == 2: # current region has 2 neighbours
-            region = region.rightN[0] if self._is_below(region.rightp, seg) else region.rightN[1]
+        if len(region.data.rightN) == 2: # current region has 2 neighbours
+            region = region.data.rightN[0] if self._is_below(region.data.rightp, seg) else region.data.rightN[1]
         else: # else it has 1 neighbor
-            region = region.rightN[0]
-        return region.data
+            region = region.data.rightN[0]
+        return region # Returns the node of the region
 
     def _follow_segment(self, node, seg):
-        regions = [node.data] # appends delta_0 - this region contains p
-        region = self._next_neighbour(node.data, seg)
+        regions = [node] # appends delta_0 - this region contains p
+        region = self._next_neighbour(node, seg) 
 
         while seg.end.x > region.rightp.x: # appends delta_1 -> delta_k-1 - these regions are intersected by s_i
             regions.append(region)
@@ -31,38 +37,79 @@ class SearchStructure:
         return regions
 
     def insert(self, seg):
-        # New nodes
-        node = self._find_region(seg.start) # get region for point (Delta_0) - Trapezoid is node.data
+        node = self._find_region(seg.start) # get region for point (delta_0) - Trapezoid is node.data
         q_node = Node(seg.end)
         seg_node = Node(seg)
-        
-        # Trapezoid border data
-        upper = node.data.upper
-        lower = node.data.lower
-        leftp = node.data.leftp
-        rightp = node.data.rightp  
 
-        xs = self._is_below(seg.end, upper) and not self._is_below(seg.end, lower) and leftp < seg.end < rightp # FIXME: remove this 
+        # FIXME: remove this
+        xs = self._is_below(seg.end, node.data.upper) and not self._is_below(seg.end, lower) and leftp < seg.end < rightp 
+        
         # CASE WHERE ENTIRE LINE SEGMENT IS not CONTAINED WITHIN A SINGLE REGION/TRAPEZOID
         if not xs: # if not the case below???
-            traps = self._follow_segment(node, seg)
-            
-            first, last = traps[0], traps[:-1]
-            # handle first and last traps
-            B = seg_node.left = Node(Trapezoid(upper, seg, seg.start, first.rightp)) # Creates B 
-            first.rightp = seg.start # Change delta_0's rightp to seg start => A
-            C = seg_node.right = Node(Trapezoid(seg, lower, seg.start, self._next_neighbour(first, seg).rightp))
+            traps = self._follow_segment(node, seg) # traps is a list of nodes of regions
 
+            first, last = traps[0].data, traps[:-1].data # delta_0 and delta_k
+            # Make three new trapezoids (for delta_0 aka first trapezoid) - FIXME: DONE MAYBE?
+            Ap = Node(Trapezoid(first.upper, first.lower, first.leftp, seg.start))
+            Bp = Node(Trapezoid(first.upper, seg, seg.start, first.rightp)) # upper and rightp from delta_0, lower is segment, leftp is start of segment
+            Cp = Node(Trapezoid(seg, first.lower, seg.start, first.rightp)) # lower and rightp from delta_0, upper is segment, leftp is start of segment
 
+            # Rearrange accoringly in SS for p
+            node.overwrite(seg.start, Ap, seg_node) # change delta_0 node to P
+            seg_node.left = Bp # above
+            seg_node.right = Cp # below
 
+            # Make three new trapezoids (for delta_k aka last trapezoid) - FIXME: DONE MAYBE?
+            Aq = Node(Trapezoid(last.upper, last.lower, seg.end, last.rightp))
+            Bq = Node(Trapezoid(last.upper, seg, last.leftp, seg.end)) # upper and leftp from delta_k, lower is segment, rightp is end of segment
+            Cq = Node(Trapezoid(seg, last.lower, last.leftp, seg.end)) # lower and leftp from delta_k, upper is segment, rightp is end of segment 
+
+            # Rearrange accoringly in SS for q
+            node.overwrite(seg.end, seg_node, Aq) # change delta_k node to Q
+            seg_node.left = Bq # above
+            seg_node.right = Cq # below
 
             # Iteratively handle the middle traps
-            for trap in traps[1:-1]:
-                pass # do the merge thing and the new traps
+            for trap in traps[1:-1]: # if it helps; "trap" is the same as "delta_i"
+                B, C = Bp, Cp # starting trapezoid to try and merge with the next trapezoids
+                current = trap.data
+                trap.data = seg
+                # split current trapezoid 
+                above = Trapezoid(current.upper, seg, current.leftp, current.rightp)
+                below = Trapezoid(seg, current.lower, current.leftp, current.rightp)
+                # merging/making trapezoids above/below the segment
+                if B.upper == above.upper:
+                    B.rightp = above.rightp # merge B with next region above the segment - "above" dies
+                    current.upper = seg
+                    trap.left = B
+                else: # if not merging with B save current as trapezoid as below as well as the next trapezoid to try and merge with
+                    B = trap.left = current
+                    
+                if C.lower == below.lower:
+                    C.rightp = below.rightp # merge C with next region below the segment - "below" dies
+                    current.lower = seg
+                    trap.right = C
+                else: # if not merging with C save current as trapezoid as below as well as the next trapezoid to try and merge with
+                    C = trap.right = current
 
-        
+            
+            
+
+                # NOTE - REMEMBER TO MERGE WITH Bq AND Cq AFTER DOING THE MIDDLE STUFF
+                # NOTE - REMEMBER TO ADD NEIGHBOUR-RELATIONS TO TRAPEZOIDS
+
+
+
+
+
+        # FIXME - ABSTRACT THIS TO "SIGNLE_TRAP_INSERTION(...)"
         # CASE WHERE ENTIRE LINE SEGMENT IS CONTAINED WITHIN A SINGLE REGION/TRAPEZOID
-        elif self._is_below(seg.end, upper) and not self._is_below(seg.end, lower) and leftp < seg.end < rightp:
+        elif self._is_below(seg.end, node.data.upper) and not self._is_below(seg.end, lower) and leftp < seg.end < rightp:
+            # Trapezoid border data
+            upper = node.data.upper
+            lower = node.data.lower
+            leftp = node.data.leftp
+            rightp = node.data.rightp  
             # Make Trapezoid and assign to p_i.left (A)
             A = node.left = Node(Trapezoid(upper, lower, leftp, seg.start))
             node.right = q_node
